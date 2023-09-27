8b40f1eef511895bca6f5a9bbd3eba21825991d2
axmaijaz <asmaijaz456@gmail.com>
2023-09-27 12:49:12 +0500

    build-checkbug-nbr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export abcs class AppComponent {
   title = 'lunar';
 }
